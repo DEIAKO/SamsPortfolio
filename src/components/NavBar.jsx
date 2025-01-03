7e58e2f78872285a46ffa5fdd229b860cff59d92
Samuel Tvmu <79530541+DEIAKO@users.noreply.github.com>
2025-01-03 13:59:18 +0800

    routing done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <nav className="bg-black bg-opacity-80 fixed top-0 left-0 right-0 p-4">
+    <nav className="bg-black z-20 bg-opacity-80 fixed top-0 left-0 right-0 p-2">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-white text-3xl font-bold">Sam's.</h1>
-        <ul className="flex space-x-20">
+        <ul className="flex space-x-20 font-mono">
           <li><a href="/" className="text-white">Home</a></li>
           <li><a href="/cart" className="text-white">About Me</a></li>
           <li><a href="/cart" className="text-white">Skills</a></li>
-          <li><a href="/cart" className="text-white">Hire Me</a></li>
+          <li><a href="/cart" className="text-white">Hire Me.</a></li>
         </ul>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import ProductList from '../components/ProductList';
 
 const Home = () => {
   return (
-    <div>
-      <img src="C:\Users\admin\Desktop\reactjobs\SamsPortfolio\samsportfolio\src\assets\samprofile.jpg" alt="" />
-      <h1 className="position-absolute mt-60 text-4xl text-white font-semibold text-center mb-8">Warmly welcome...</h1>
-      <h2 className="text-2xl text-white font-semibold text-center mb-8">Myanmar, Developer from Philippine.</h2>
-      <p className='text-justify text-white mx-80 my-36'>🚀🙋‍♂️I am Samuel Htamu, a passionate and results-driven front-end developer with expertise in React.js and JavaScript. With a strong foundation in building dynamic, user-friendly, and responsive web applications, I specialize in creating seamless digital experiences. My proficiency in modern JavaScript frameworks, component-based architecture, and performance optimization enables me to deliver scalable solutions that meet user needs and business goals. Dedicated to continuous learning and innovation, I am eager to contribute to impactful projects and help bring ideas to life through exceptional web development.</p>
+    <body>
+      <div>
+      <img className='w-1/4 relative top-40 left-1/2 border-4 rounded-full drop-shadow-lg z-0' src="./src/assets/samprofile.jpg" alt="" />
+      <h1 className="position-realative mt-0 ml-16 text-6xl text-orange-400 font-semibold  mb-2">Warmly welcome...</h1>
+      <h2 className="position-relative text-2xl mt-0 ml-20 text-slate-50 font-semibold">Myanmar, Developer from Philippine.</h2>
+      </div>
+      
+      <p className='position-relative font-mono text-justify text-slate-50 mx-52 text-xl mt-40 mb-28'> 🚀🙋‍♂️I am Samuel Htamu, a passionate and results-driven front-end developer with expertise in React.js and JavaScript. With a strong foundation in building dynamic, user-friendly, and responsive web applications, I specialize in creating seamless digital experiences. My proficiency in modern JavaScript frameworks, component-based architecture, and performance optimization enables me to deliver scalable solutions that meet user needs and business goals. Dedicated to continuous learning and innovation, I am eager to contribute to impactful projects and help bring ideas to life through exceptional web development.</p>
       <p className='text-center mb-8 text-white'>Here you can browse through our collection of pets and find the perfect pet for your needs.</p>
       <ProductList />
       <ProductList />
@@ -16,8 +19,8 @@ const Home = () => {
       Eligendi maxime mollitia quia? Distinctio deleniti esse, mollitia culpa maiores dolores possimus quos dolorum repellendus itaque assumenda accusamus cum eveniet commodi magnam officiis. Et voluptatibus earum provident nostrum quae illo?
       Unde voluptatem ullam tempore voluptatibus praesentium laudantium beatae. Dolore minus vitae molestiae! Ex magni qui sunt iste et numquam exercitationem tempora. Nemo tempore nihil pariatur blanditiis, facere recusandae omnis repellat!
       Voluptate porro autem magnam ut, repellat eaque quae quis tempore, iste assumenda modi harum totam nam libero exercitationem esse. Impedit nesciunt nihil iusto vero ipsum tenetur eos, quasi quas fugiat.</p>
-    </div>
+    </body>
   );
-}
+};
 
 export default Home;
